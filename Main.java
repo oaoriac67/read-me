676e8cf633e4a619007748be548b45a4165996fa
oaoriac67 <cairooliver2000@gmail.com>
2026-06-18 14:25:11 +0000

    Day 6

--- Main.java
+++ Main.java
@@ -1,1 +1,5 @@
-
+public class Main{
+    public static void main(String[] args) {
+        Toy toy1 = new toy();
+    }
+}
--- Toy.java
+++ Toy.java
@@ -1,12 +1,34 @@
+/**
+ * Toy Object
+ * has a property of name, brand, price, and quantity
+ * with a method of setPrice() where you can change
+ * the price state of Toy Object.
+ */
+
 public class Toy {
-    public String name;
+    public String name="Gundam";
     public String brand;
     public double price;
     public int quantity;
+    /* this
+    * is
+    * a 
+    * multi
+    * line
+    * comment 
+    */
+
+
+    // set the amount of toy to the argument example setPrice(1000)
+    void setPrice(double price){
+        this.price = price;
+    }
     //psvm - public static void main(String[] args) {}
     public static void main(String[] args) {
         //sout System.out.println("hello java trainees");
         System.out.println("hello java trainees");
+        Toy toy1 = new Toy();
+        System.out.println(toy1.name);
     
 }
 
