d1cc3ef3af533b363aaf09018c33ff0bba7ce336
oaoriac67 <cairooliver2000@gmail.com>
2026-06-20 01:56:42 +0000

    Day 12

--- chapter1/PrimitivesLesson.java
+++ chapter1/PrimitivesLesson.java
@@ -30,7 +30,7 @@ public class PrimitivesLesson {
         System.out.println(123_345.678_9);
         //additional info
         System.out.println(10);
-        System.out.println(050); //0## 0 prefix is octal    
+        System.out.println(050); //0## 0 prefix is octal
         System.out.println(0xa); //0X## or 0x## 0x prefix is hexadecimal
         System.out.println(0B101);//0B## or 0b## 0b prefix is binary
 
